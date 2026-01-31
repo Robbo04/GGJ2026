8d042367d7274ebaf85d28e9281ae4fa15bad95c
Sam Robertson <samrobertson5@outlook.com>
2026-01-31 12:09:55 +0000

    interactions + playerInputActions
    Highlights object when looked at
    Pressing interact invokes an event. clicking deletes object currently

---
+++ GGJ2026_deepseadiver/Assets/Scripts/Destroy.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    void OnEnable()
+    {
+        Destroy(gameObject);
+    }
+}
--- GGJ2026_deepseadiver/Assets/Scripts/Player/PlayerInteraction.cs
+++ GGJ2026_deepseadiver/Assets/Scripts/Player/PlayerInteraction.cs
@@ -1,61 +1,82 @@
-// using UnityEngine;
-// using System.Collections.Generic;
-// using System.Collections;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.InputSystem;
 
-// public class PlayerInteraction : MonoBehaviour
-// {
-//     public float reachRange = 3f;
-//     Interactable currentInteractable;
+public class PlayerInteraction : MonoBehaviour
+{
+    public float reachRange = 3f;
+    Interactable currentInteractable;
+    
+    private PlayerInputActions playerControls;
 
-//     // Update is called once per frame
-//     void Update()
-//     {
-//         CheckInteraction();
-//         if (Input.GetKeyDown(KeyCode.E))
-//         {
-//             if (currentInteractable != null)
-//             {
-//                 currentInteractable.Interact();
-//             }
-//         }
-//     }
+    void Awake()
+    {
+        playerControls = new PlayerInputActions();
+    }
 
-//     void CheckInteraction()
-//     {
-//         RaycastHit hit;
-//         Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
-//         if (Physics.Raycast(ray, out hit, reachRange))
-//         {
-//             if (hit.collider.tag == "Interactable")
-//             {
-//                 Interactable newInteractable = hit.collider.GetComponent<Interactable>();
+    void OnEnable()
+    {
+        playerControls.PlayerController.Enable();
+        playerControls.PlayerController.Interact.performed += OnInteract;
+    }
 
-//                 if (newInteractable.enabled)
-//                 {
-//                     SetNewInteractable(newInteractable);
-//                 }
+    void OnDisable()
+    {
+        playerControls.PlayerController.Interact.performed -= OnInteract;
+        playerControls.PlayerController.Disable();
+    }
 
-//             }
-//             else
-//             {
-//                 ClearCurrentInteractable();
-//             }
-//         }
-//     }
+    private void OnInteract(InputAction.CallbackContext context)
+    {
+        if (currentInteractable != null)
+        {
+            currentInteractable.Interact();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckInteraction();
+    }
+
+    void CheckInteraction()
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+        if (Physics.Raycast(ray, out hit, reachRange))
+        {
+            if (hit.collider.tag == "Interactable")
+            {
+                Interactable newInteractable = hit.collider.GetComponent<Interactable>();
+
+                if (newInteractable.enabled)
+                {
+                    SetNewInteractable(newInteractable);
+                }
+
+            }
+            else
+            {
+                ClearCurrentInteractable();
+            }
+        }
+    }
 
 
-//     void SetNewInteractable(Interactable newInteractable)
-//     {
-//         currentInteractable = newInteractable;
-//         currentInteractable.EnableOutline();
-//     }
+    void SetNewInteractable(Interactable newInteractable)
+    {
+        currentInteractable = newInteractable;
+        currentInteractable.EnableOutline();
+    }
 
-//     void ClearCurrentInteractable()
-//     {
-//         if (currentInteractable)
-//         {
-//             currentInteractable.DisableOutline();
-//             currentInteractable = null;
-//         }
-//     }
-// }
+    void ClearCurrentInteractable()
+    {
+        if (currentInteractable)
+        {
+            currentInteractable.DisableOutline();
+            currentInteractable = null;
+        }
+    }
+}
