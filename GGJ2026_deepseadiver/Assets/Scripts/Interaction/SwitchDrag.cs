9e28c697936ed4b532babb2bdf73660b4c952dd4
RileyM7 <riley2.macfarlane@live.uwe.ac.uk>
2026-02-01 11:59:07 +0000

    Revert "Merge branch 'main' of https://github.com/Robbo04/GGJ2026"
    This reverts commit a1fd330698f1d4b9b577aca88e47f05bd31bdf3f, reversing
    changes made to 9ee2e0b66c5822cf5eb3d60c3bcea657a5569dd7.

--- GGJ2026_deepseadiver/Assets/Scripts/Interaction/SwitchDrag.cs
+++ GGJ2026_deepseadiver/Assets/Scripts/Interaction/SwitchDrag.cs
@@ -108,17 +108,6 @@ public class SwitchDrag : Interactable
         {
             // State changed, invoke event
             Interact();
-            
-            if (isOn)
-            {
-                PulledDown();
-            }
         }
-    }
-    
-    void PulledDown()
-    {
-        // Lever has been pulled down (toggled on)
-        Debug.Log("Lever pulled down!");
     }
 }
