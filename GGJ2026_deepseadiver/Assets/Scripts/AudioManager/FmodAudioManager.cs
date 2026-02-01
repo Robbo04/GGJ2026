9ee2e0b66c5822cf5eb3d60c3bcea657a5569dd7
RileyM7 <riley2.macfarlane@live.uwe.ac.uk>
2026-02-01 11:45:31 +0000

    Begun implementing sounds

---
+++ GGJ2026_deepseadiver/Assets/Scripts/AudioManager/FmodAudioManager.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using FMODUnity;
+using FMOD.Studio;
+
+public class FmodAudioManager : MonoBehaviour
+{
+  public static FmodAudioManager Instance { get; private set; }
+  public EventInstance ambienceEventInstance;
+
+  public EventInstance areaEventInstance;
+
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+           Debug.LogError("Multiple instances of FmodAudioManager detected!"); 
+        }
+        Instance = this;
+    }
+
+    public void PlayOneShot(EventReference sound, Vector3 position)
+    {
+        RuntimeManager.PlayOneShot(sound, position);
+    }
+
+    public void SetMusicArea(Area area)
+    {
+        areaEventInstance.setParameterByName("Area", (float) area);
+    }
+
+    public void SetAmbienceParameter(string paramName, float value)
+    {
+        ambienceEventInstance.setParameterByName(paramName, value);
+    }
+}
