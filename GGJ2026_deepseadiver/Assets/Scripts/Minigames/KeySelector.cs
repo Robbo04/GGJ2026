b2b24e9278fa0efd79b3b1fffd5209227f9942d9
RileyM7 <riley2.macfarlane@live.uwe.ac.uk>
2026-03-18 12:41:59 +0000

    touched up some Fmod
    - slightly changed the entering into water event
    - created glass cracking audio

--- GGJ2026_deepseadiver/Assets/Scripts/Minigames/KeySelector.cs
+++ GGJ2026_deepseadiver/Assets/Scripts/Minigames/KeySelector.cs
@@ -66,7 +66,7 @@ public class KeySelector : MonoBehaviour
         charToSprite.Add('Y', spriteY);
         charToSprite.Add('Z', spriteZ);
 
-        moveCrainSound = RuntimeManager.CreateInstance("event:/Cage Effects/CageDownFixed");
+        moveCrainSound = RuntimeManager.CreateInstance("event:/Cage Effects/CageIntoWater");
         buttonSound = RuntimeManager.CreateInstance("event:/Minigame Oneshots/Button");
         showLetterSound = RuntimeManager.CreateInstance("event:/Minigame Oneshots/showLetters");
         
