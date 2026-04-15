d8cdf5f66b444056f68dc91ac7b73031375f9b88
CharlieC <jartercarter2020@gmail.com>
2026-04-15 16:08:43 +0100

    CC & SR - Crack Mechanic V2
    - Added code set up for new materials to be added in, more states for cracks and they the material of the glass will change rather than an overlay.

--- GGJ2026_deepseadiver/Assets/Scripts/Player/Crack.cs
+++ GGJ2026_deepseadiver/Assets/Scripts/Player/Crack.cs
@@ -3,12 +3,14 @@ using UnityEngine;
 public class Crack : MonoBehaviour
 {
     public bool isCracked = false; // Flag to track if the helmet is cracked
+    public GameObject[] Windows;
+    public int Health = 2;
     
     public void RemoveCracked()
     {
-        if(isCracked)
+        if(!isCracked)
         {
-            isCracked = false;
+            Health = 2;
             print("Crack removed");
             GetComponent<Renderer>().enabled = false;
             GetComponent<Collider>().enabled = false;
@@ -19,12 +21,30 @@ public class Crack : MonoBehaviour
     {
         if (!isCracked)
         {
-            isCracked = true;
-            print("Crack added");
-            // Add crack logic
-            GetComponent<Renderer>().enabled = true;
-            GetComponent<Collider>().enabled = true;
-            //FMOD Crack sound play Front Side
+            Health =- 1;
+            if (Health == 0)
+            {
+                DestroyWindow();
+            }
+            else
+            {
+                print("Crack added");
+                // Add crack logic
+                GetComponent<Renderer>().enabled = true;
+                GetComponent<Collider>().enabled = true;
+                //FMOD Crack sound play Front Side
+            }
+            
+           
         }
+    }
+
+    public void DestroyWindow()
+    {
+        isCracked = true;
+        GetComponent<Renderer>().enabled = true;
+        GetComponent<Collider>().enabled = false;
+        //audio break
+        //visual cue here
     }
 }
--- GGJ2026_deepseadiver/Assets/Scripts/Player/HelmetCrack.cs
+++ GGJ2026_deepseadiver/Assets/Scripts/Player/HelmetCrack.cs
@@ -12,8 +12,12 @@ using FMODUnity;
 public class HelmetCrack : MonoBehaviour
 {
     public GameObject[] CrackedWindow; //list of gameobjects
+    
+    public Material DefaultWindowMat;
+    public Material MildCrackedWindowMat;
+    public Material HeavyCrackedWindowMat;
 
-    [SerializeField] public FMOD.Studio.EventInstance crackSound;
+    [SerializeField] public FMOD.Studio.EventInstance crackSound; //FMOD event for crack sound.
     int crackCount;
     float elapsedTime;
 
@@ -21,34 +25,52 @@ public class HelmetCrack : MonoBehaviour
 
     void Start()
     {
+        //reset crack counter to 0
         crackCount = 0;
     }
 
     void FixedUpdate()
     {
+        //if all the windows have broken
         if(crackCount < 3)
         {
+        //increasing the timer with delta time
         elapsedTime += Time.deltaTime;
+        //if the timer exceeds 5 seconds
         if (elapsedTime > 5)
             {
+                //SRAND if the window should break or not
                 int randomNumber = Random.Range(0,3);
                 print ("RandomNumber " + randomNumber);
-                if (randomNumber == 2)
+                if (randomNumber == 2) //when SRAND is equal to 2 proceed to crack a window
                 {
-                    int randomCrack;
-                    do
+                    int randomCrack; //initialise SRAND variable
+                    do //find a random window that is not cracked
                     {
-                        randomCrack = Random.Range(0, CrackedWindow.Length);
+                        randomCrack = Random.Range(0, CrackedWindow.Length); //SRAND find random window in gameobject list
                         print("RandomCrack " + randomCrack);
-                    } while (CrackedWindow[randomCrack].GetComponent<Crack>().isCracked);
-                    CrackedWindow[randomCrack].GetComponent<Crack>().AddCracked();
-                    crackSound = RuntimeManager.CreateInstance("event:/Minigame Oneshots/Cracking");
-                    FmodAudioManager.Instance.PlayOneShot(crackSound, transform.position);
+                    } while (CrackedWindow[randomCrack].GetComponent<Crack>().isCracked); //checks to see if window found is cracked
+                    //window is found, now do this
+                    CrackedWindow[randomCrack].GetComponent<Crack>().AddCracked(); //trigger add cracked function
+                    //Trigger FMOD functionality.
+                    crackSound = RuntimeManager.CreateInstance("event:/Minigame Oneshots/Cracking"); 
+                    FmodAudioManager.Instance.PlayOneShot(crackSound, transform.position); 
                     Debug.Log("crackedSound");
+                    
+                    switch (CrackedWindow[randomCrack].GetComponent<Crack>().Health)    {
+                        case 0: //Set window to be cracked material Default:
+                        break;
+                        case 1: //Set window to be cracked material light crack:
+                        break;
+                        case 2: //Set window to be cracked material heavy:
+                        break;
+                    }
                 }
                 elapsedTime = 0;
             }
         }
+
+        //
     }
 
     void Update()
