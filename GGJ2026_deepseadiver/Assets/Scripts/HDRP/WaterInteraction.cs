e18304dc7c24d5833d53717e29717626ddae3077
Sam Robertson <samrobertson5@outlook.com>
2026-06-10 17:00:32 +0100

    underwater Script
    uses height as judgement, will need to look into water system later for when waves are crazier and for things like splashes

--- GGJ2026_deepseadiver/Assets/Scripts/HDRP/WaterInteraction.cs
+++ GGJ2026_deepseadiver/Assets/Scripts/HDRP/WaterInteraction.cs
@@ -3,24 +3,43 @@ using UnityEngine;
 
 public class WaterInteraction : MonoBehaviour
 {
-    public GameObject hitObject;
-    public Vector3 collision = Vector3.zero;
+    public GameObject Ocean;
+    public float waterLevel;
+    public GameObject playerWaterHeightMarker;
+    public bool isUnderwater => this.transform.position.y < waterLevel;
 
-    void Update()
+    void Start()
     {
-        var ray = new Ray(this.transform.position + transform.forward, this.transform.forward);
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, 100))
+        if (Ocean != null)
         {
-            hitObject = hit.transform.gameObject;
-            collision = hit.point;
-            Debug.DrawRay(transform.position, transform.forward * 100, Color.green);
+            waterLevel = Ocean.GetComponent<Transform>().position.y;
         }
     }
 
     void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(collision, 0.2f);
+        Gizmos.DrawWireSphere(playerWaterHeightMarker != null ? playerWaterHeightMarker.transform.position : Vector3.zero, 0.2f);
     }
+
+    // public GameObject hitObject;
+    // public Vector3 collision = Vector3.zero;
+
+    // void Update()
+    // {
+    //     var ray = new Ray(this.transform.position + transform.forward, this.transform.forward);
+    //     RaycastHit hit;
+    //     if (Physics.Raycast(ray, out hit, 100))
+    // {
+    //         hitObject = hit.transform.gameObject;
+    //         collision = hit.point;
+    //         Debug.DrawRay(transform.position, transform.forward * 100, Color.green);
+    //     }
+    // }
+
+    // void OnDrawGizmos()
+    // {
+    //     Gizmos.color = Color.red;
+    //     Gizmos.DrawWireSphere(collision, 0.2f);
+    // }
 }
